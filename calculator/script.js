e253ada5b3e7dd6b49579f7ec7111ea4964d97b7
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-09-24 15:23:06 +1200

    feat: add keyboard(numpad) input

--- calculator/script.js
+++ calculator/script.js
@@ -9,6 +9,71 @@ const equalsBtn = document.querySelector("[data-equals]");
 const operationBtn = document.querySelectorAll("[data-operation]");
 const numberBtn = document.querySelectorAll("[data-number]");
 
+const keyCodeNumpad = {
+  number: [
+    "Numpad0",
+    "Numpad1",
+    "Numpad2",
+    "Numpad3",
+    "Numpad4",
+    "Numpad5",
+    "Numpad6",
+    "Numpad7",
+    "Numpad8",
+    "Numpad9",
+    "NumpadDecimal",
+  ],
+  operation: ["NumpadAdd", "NumpadSubtract", "NumpadMultiply", "NumpadDivide"],
+  delete: ["Backspace"],
+  compute: ["NumpadEnter"],
+};
+
+const calculator = new Calculator(
+  previousOperandInput,
+  currentOperandInput,
+  resultationInput
+);
+
+const getValueOfKeyCode = (keycode) => {
+    switch (keycode) {
+      case "Numpad0":
+        return "0";
+      case "Numpad1":
+        return "1";
+      case "Numpad2":
+        return "2";
+      case "Numpad3":
+        return "3";
+      case "Numpad4":
+        return "4";
+      case "Numpad5":
+        return "5";
+      case "Numpad6":
+        return "6";
+      case "Numpad7":
+        return "7";
+      case "Numpad8":
+        return "8";
+      case "Numpad9":
+        return "9";
+      case "NumpadDecimal":
+        return "."
+    }
+};
+
+const getOperationOfKeyCode = (keycode) => {
+  switch (keycode) {
+    case "NumpadAdd":
+      return "+";
+    case "NumpadSubtract":
+      return "-";
+    case "NumpadMultiply":
+      return "*";
+    case "NumpadDivide":
+      return "÷";
+  }
+};
+
 numberBtn.forEach((e) => {
   e.addEventListener("click", () => {
     calculator.setValue(e.textContent);
@@ -53,3 +118,25 @@ equalsBtn.addEventListener("click", () => {
   calculator.updateDisplay();
   equalsBtn.blur()
 })
+
+
+document.addEventListener("keyup", (evt) => {
+ if (keyCodeNumpad.number.includes(evt.code)) {
+   calculator.setValue(getValueOfKeyCode(evt.code));
+ }
+
+ if (keyCodeNumpad.operation.includes(evt.code)) {
+   calculator.setOperation(getOperationOfKeyCode(evt.code));
+ }
+
+ if (keyCodeNumpad.compute.includes(evt.code)) {
+   calculator.compute();
+ }
+
+ if (keyCodeNumpad.delete.includes(evt.code)) {
+   calculator.delete();
+ }
+  
+  calculator.updateDisplay();
+});
+
