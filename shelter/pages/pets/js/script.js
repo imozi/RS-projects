aaa01f7a22820ef49edf04e8d446d252e5458429
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-10-23 15:43:46 +1200

    feat: add random animals & add pagination

--- shelter/pages/pets/js/script.js
+++ shelter/pages/pets/js/script.js
@@ -14,7 +14,7 @@
 
     getTemplateCard(animal, id) {
       return `
-    <article class="our-friends__item animal-card" data-key="${id}">
+    <article class="our-friends__item animal-card animal-card--animate-fade" data-key="${id}">
       <div class="animal-card__photo">
         <img src="${animal.img}" alt="${animal.name}">
       </div>
@@ -69,9 +69,7 @@
         }
       };
 
-      this.container.addEventListener("click", (evt) => {
-        onClickCard(evt);
-      });
+      this.container.addEventListener("click", onClickCard);
     }
 
     onClickCloseModalInfo() {
@@ -102,9 +100,9 @@
       modalPetCloseBtn.addEventListener("click", onClickCloseBtn);
     }
 
-    renderCards() {
-      const card = this.animals.reduce((a, e, i) => {
-        return (a += this.getTemplateCard(e, i));
+    renderCards(animals = this.animals) {
+      const card = animals.reduce((a, e, i) => {
+        return (a += this.getTemplateCard(e, e.id || i));
       }, ``);
 
       this.container.insertAdjacentHTML("afterBegin", card);
@@ -121,13 +119,234 @@
     }
   }
 
+  class Pagination extends AnimalCard {
+    constructor(options) {
+      super(options.animals, options.container);
+      this.animals = options.animals;
+      this.container = options.container;
+      this.pagination = options.pagination;
+      this.breakpoints = options.breakpoints;
+      this.currentPage = 1;
+      this.setCurrentPage();
+      this.renderCardsNewBreakpoints();
+    }
+
+    getQuantityPage(quantity) {
+      this.quantityPage = Math.ceil(this.animals.length / quantity);
+    }
+
+    setCurrentPage() {
+      const pagination = document.querySelector(this.pagination);
+
+      this.paginationCurrentPage = pagination.querySelector(".pagination__current-page");
+      this.paginationCurrentPage.textContent = this.currentPage;
+    }
+
+    onClickBtn() {
+      const pagination = document.querySelector(this.pagination);
+      const btnPrevLast = pagination.querySelector(".pagination__btn--prev-last");
+      const btnPrev = pagination.querySelector(".pagination__btn--prev");
+      const btnNext = pagination.querySelector(".pagination__btn--next");
+      const btnNextLast = pagination.querySelector(".pagination__btn--last-next");
+
+      const disabledPrevBtn = () => {
+        btnPrevLast.disabled = true;
+        btnPrev.disabled = true;
+      };
+
+      disabledPrevBtn();
+
+      const disabledNextBtn = () => {
+        btnNext.disabled = true;
+        btnNextLast.disabled = true;
+      };
+
+      const enablePrevBtn = () => {
+        btnPrevLast.disabled = false;
+        btnPrev.disabled = false;
+      };
+
+      const enableNextBtn = () => {
+        btnNext.disabled = false;
+        btnNextLast.disabled = false;
+      };
+
+      const onClickNextBtn = () => {
+        this.currentPage++;
+        this.setCurrentPage();
+        this.renderCardsNewPage();
+
+        if (this.currentPage === this.quantityPage) {
+          disabledNextBtn();
+          return;
+        }
+
+        enablePrevBtn();
+      };
+
+      const onClickPrevBtn = () => {
+        this.currentPage--;
+        this.setCurrentPage();
+        this.renderCardsNewPage();
+
+        if (this.currentPage === 1) {
+          disabledPrevBtn();
+          return;
+        }
+
+        enableNextBtn();
+      };
+
+      const onClickPrevLastBtn = () => {
+        this.currentPage = 1;
+        this.setCurrentPage();
+        this.renderCardsNewPage();
+        disabledPrevBtn();
+        enableNextBtn();
+      };
+
+      const onClickNextLastBtn = () => {
+        this.currentPage = this.quantityPage;
+        this.setCurrentPage();
+        this.renderCardsNewPage();
+        disabledNextBtn();
+        enablePrevBtn();
+      };
+
+      btnPrevLast.addEventListener("click", onClickPrevLastBtn);
+      btnPrev.addEventListener("click", onClickPrevBtn);
+      btnNext.addEventListener("click", onClickNextBtn);
+      btnNextLast.addEventListener("click", onClickNextLastBtn);
+    }
+
+    renderCardsNewBreakpoints() {
+      const bodyWidth = document.body.offsetWidth;
+
+      if (bodyWidth >= 320 && bodyWidth < 768) {
+        if (this.breakpoints[320].isActive) return;
+
+        this.getQuantityPage(this.breakpoints[320].showItems);
+        this.quantityItem = this.breakpoints[320].showItems;
+        this.resetPagination();
+        this.renderCardsNewPage();
+
+        this.breakpoints[320].isActive = true;
+        this.breakpoints[768].isActive = false;
+        this.breakpoints[1280].isActive = false;
+      }
+
+      if (bodyWidth >= 768 && bodyWidth < 1280) {
+        if (this.breakpoints[768].isActive) return;
+
+        this.getQuantityPage(this.breakpoints[768].showItems);
+        this.quantityItem = this.breakpoints[768].showItems;
+        this.resetPagination();
+        this.renderCardsNewPage();
+
+        this.breakpoints[320].isActive = false;
+        this.breakpoints[768].isActive = true;
+        this.breakpoints[1280].isActive = false;
+      }
+
+      if (bodyWidth >= 1280) {
+        if (this.breakpoints[1280].isActive) return;
+
+        this.getQuantityPage(this.breakpoints[1280].showItems);
+        this.quantityItem = this.breakpoints[1280].showItems;
+        this.resetPagination();
+        this.renderCardsNewPage();
+
+        this.breakpoints[768].isActive = false;
+        this.breakpoints[1280].isActive = true;
+      }
+    }
+
+    resetPagination() {
+      const pagination = document.querySelector(this.pagination);
+      const btnPrevLast = pagination.querySelector(".pagination__btn--prev-last");
+      const btnPrev = pagination.querySelector(".pagination__btn--prev");
+      const btnNext = pagination.querySelector(".pagination__btn--next");
+      const btnNextLast = pagination.querySelector(".pagination__btn--last-next");
+
+      btnPrevLast.disabled = true;
+      btnPrev.disabled = true;
+      btnNext.disabled = false;
+      btnNextLast.disabled = false;
+
+      this.currentPage = 1;
+      this.setCurrentPage();
+    }
+
+    renderCardsNewPage() {
+      const newAnimals = [];
+      const oldItems = this.container.querySelectorAll(".our-friends__item");
+
+      oldItems.forEach(e => e.remove());
+
+      for (
+        var i = (this.currentPage - 1) * this.quantityItem;
+        i < this.currentPage * this.quantityItem && i < this.animals.length;
+        i++
+      ) {
+        this.animals[i].id = i;
+        newAnimals.push(this.animals[i]);
+      }
+
+      this.renderCards(newAnimals);
+    }
+
+    onResizeWindowRenderCardsNewPage() {
+      this.renderCardsNewBreakpoints();
+    }
+
+    init() {
+      this.renderCardsNewBreakpoints();
+      this.onClickBtn();
+      window.addEventListener("resize", this.onResizeWindowRenderCardsNewPage.bind(this));
+    }
+  }
+
   const url =
     "https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/markups/level-2/shelter/pets.json";
-  const animals = await fetch(url).then((res) => res.json());
-  
-  const animalCard = new AnimalCard({ animals: animals, container: animalsContainer });
-  animalCard.renderCards()
-  animalCard.onClickCardGetInfo();
+  const data = await fetch(url).then((res) => res.json());
+  const animals = (() => {
+    let temp = [];
+
+    for (let i = 0; i < 6; i++) {
+      const newData = data;
+
+      for (let j = data.length; j > 0; j--) {
+        const index = Math.floor(Math.random() * j);
+        const randomElement = newData.splice(index, 1)[0];
+        newData.push(randomElement);
+      }
+
+      temp = [...temp, ...newData];
+    }
+
+    return temp;
+  })();
+
+
+  const pagination = new Pagination({
+    animals: animals,
+    container: animalsContainer,
+    pagination: ".pagination",
+    breakpoints: {
+      320: {
+        showItems: 3,
+      },
+      768: {
+        showItems: 6,
+      },
+      1280: {
+        showItems: 8,
+      },
+    },
+  });
+
+  pagination.init();
+  pagination.onClickCardGetInfo();
 
   const onClickBtn = () => {
     body.classList.toggle("open-menu");
