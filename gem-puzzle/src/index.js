301f49b8441cff7a97d02ece45d3f47d0e841571
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-11-17 19:52:27 +1200

    feat: add alert for reviewers

--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -4,3 +4,4 @@ import App from './components/app';
 const { body } = document;
 const app = new App(body);
 app.render();
+alert('Пожалуйста пока не проверяйте работу, очень сильно не успеваю, еще пару дней. Спасибо большое за понимание!');
